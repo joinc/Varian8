c84a0e0b7045703e55587b63ec8391ec2163a8f2
Yuriy Belyakov <yura.belyakov@gmail.com>
2020-01-14 21:04:43 +0600

    Update LaboratoryWork4.java

--- src/LaboratoryWork4.java
+++ src/LaboratoryWork4.java
@@ -7,79 +7,82 @@ import javax.swing.border.EtchedBorder;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.geom.Ellipse2D;
 
-public class LaboratoryWork4 {
+public class LaboratoryWork4 extends JComponent {
 
-    public static class MainFrame extends JFrame {
-        private int speed_dot = 10;
-        private JButton button_minus = new JButton("Скорость меньше");
-        private JButton button_plus = new JButton("Скорость больше");
-        private JLabel label_status = new JLabel("Скорость = " + speed_dot);
-        private JPanel panel_button = new JPanel();
-        private JPanel panel_main = new JPanel();
-        private JPanel panel_status = new JPanel();
+    public static int speed_dot = 10;
+    private int x = 199;
+    private int y = 89;
+    private int dx = 1;
+    private int dy = 1;
 
-        public void init() {
-            setTitle("Перемещение точки");
-            setSize(new Dimension(500, 500));
-            setResizable(false);
-            button_plus.addActionListener(new PlusActionListener());
-            button_minus.addActionListener(new MinusActionListener());
-            panel_button.add(button_minus);
-            panel_button.add(button_plus);
-            panel_status.add(label_status);
-            panel_main.setBorder(new EtchedBorder());
-            panel_status.setBorder(new EtchedBorder());
-            add(panel_button, BorderLayout.NORTH);
-            add(panel_main);
-            add(panel_status, BorderLayout.SOUTH);
-            setVisible(true);
-            setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        }
-
-        public class PlusActionListener implements ActionListener {
-            @Override
-            public void actionPerformed(ActionEvent event) {
-                if (speed_dot < 20) {
-                    speed_dot++;
-                    label_status.setText("Скорость = " + speed_dot);
-                }
-            }
-        }
-
-        public class MinusActionListener implements ActionListener {
-            @Override
-            public void actionPerformed(ActionEvent event) {
-                if (speed_dot > 1) {
-                    speed_dot--;
-                    label_status.setText("Скорость = " + speed_dot);
-                }
-            }
-        }
-
+    public LaboratoryWork4() {
+        Timer timer = new Timer(10, e -> repaint());
+        timer.start();
     }
-    /*
-        public class CircleComponent extends JPanel {
-            private int x;
-            private int y;
-            Ellipse2D.Double circle;
 
-            public CircleComponent() {
-                circle = new Ellipse2D.Double(x, y, 10, 10);
-            }
+    protected void paintComponent(Graphics g) {
+        int width = getWidth();
+        int height = getHeight();
+        g.setColor(Color.white);
+        g.fillRect(0, 0, width, height);
+        g.setColor(Color.black);
+        Graphics2D g2d = (Graphics2D) g;
+        g2d.setStroke(new BasicStroke(1f));
+        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        if ((x <= 5) || (x >= width - 5)) { dx = -1 * dx; }
+        if ((y <= 5) || (y >= height - 5)) { dy = -1 * dy; }
+        x = x + dx * speed_dot;
+        y = y + dy * speed_dot;
+        g2d.setColor(Color.RED);
+        g2d.fill(circle(x, y, 5));
+    }
 
-            public void paintComponent(Graphics g) {
-                super.paintComponent(g);
-                setBackground(Color.WHITE);
-                Graphics2D g2 = (Graphics2D) g;
-                g2.setColor(Color.RED);
-                g2.fill(circle);
+    private Shape circle(double x, double y, double r) {
+        return new Ellipse2D.Double(x - r, y - r, 2 * r, 2 * r);
+    }
+
+    public static class PlusActionListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent event) {
+            if (speed_dot < 20) {
+                speed_dot++;
+                System.out.println(speed_dot);
             }
         }
-    */
-    public static void main(String[] args) {
-        MainFrame mf = new MainFrame();
-        mf.init();
+    }
 
+    public static class MinusActionListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent event) {
+            if (speed_dot > 1) {
+                speed_dot--;
+                System.out.println(speed_dot);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                JFrame frame = new JFrame("Перемещение точки");
+                JButton button_minus = new JButton("Скорость меньше");
+                JButton button_plus = new JButton("Скорость больше");
+                button_plus.addActionListener(new PlusActionListener());
+                button_minus.addActionListener(new MinusActionListener());
+                JPanel panel_button = new JPanel();
+                panel_button.add(button_minus);
+                panel_button.add(button_plus);
+                panel_button.setBorder(new EtchedBorder());
+                frame.add(panel_button, BorderLayout.NORTH);
+                frame.add(new LaboratoryWork4());
+                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                frame.setSize(800, 600);
+                frame.setResizable(false);
+                frame.setVisible(true);
+            }
+        });
     }
 }
